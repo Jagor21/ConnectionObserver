a30c64dde0e27bc14049b26fb1c3b922406028d7
jagorkushnir <jagor.kushnir@gmail.com>
2022-10-25 19:40:08 +0400

    add NetworkStatusText composable

--- app/src/main/java/com/sgvdev/connectionobserver/MainActivity.kt
+++ app/src/main/java/com/sgvdev/connectionobserver/MainActivity.kt
@@ -5,36 +5,53 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import com.sgvdev.connectionobserver.ui.theme.ConnectionObserverTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var connectionObserver: ConnectionObserver
+    private var status = ConnectionObserver.Status.UNAVAILABLE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        connectionObserver = NetworkConnectionObserver(applicationContext)
+        observeConnection()
         setContent {
             ConnectionObserverTheme {
-
-                val status by connectionObserver.observe()
-                    .collectAsState(initial = ConnectionObserver.Status.UNAVAILABLE)
-
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = "Network status: $status"
-                    )
-                }
+                NetworkStatusText(
+                    connectionObserver.observe()
+                        .collectAsState(initial = ConnectionObserver.Status.UNAVAILABLE).value
+                )
             }
+        }
+    }
+
+    private fun observeConnection() {
+        connectionObserver = NetworkConnectionObserver(applicationContext)
+//        connectionObserver.observe().onEach {
+//            status = it
+//        }.launchIn(lifecycleScope)
+    }
+
+    @Composable
+    fun NetworkStatusText(
+        status: ConnectionObserver.Status
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Network status: $status"
+            )
         }
     }
 }
